fa00ac8020e9b13e368f9c981e2404c3374166c7
Kieranties <Kieran@Kieranties.com>
2012-11-03 08:26:30 +0000

    Reverted imagehandler changes

--- Ias-AshTag/src/org/iasess/ashtag/ImageHandler.java
+++ Ias-AshTag/src/org/iasess/ashtag/ImageHandler.java
@@ -39,7 +39,7 @@ public final class ImageHandler {
 	 * <p>
 	 * Holds the URI of the last created image by the devices camera
 	 */
-	
+	private static Uri lastCreatedImageUri;
 
 	/**
 	 * The contextual Activity for instances of this class
@@ -197,4 +197,57 @@ public final class ImageHandler {
 	public ImageHandler(Activity activity) {
 		_activity = activity;
 	}
+
+	/**
+	 * Creates an AlertDialog allowing the user to select where/how they would
+	 * select an image for the application
+	 */
+	public void showChooser() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(_activity);
+		// set items from resource list
+		String[] options = _activity.getResources().getStringArray(
+				R.array.camera_options);
+		builder.setItems(options, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int item) {
+				if (item == 0)
+					cameraIntent();
+				else
+					galleryIntent();
+			}
+		});
+		builder.create().show();
+	}
+
+	/**
+	 * Creates and executes and Intent to process an image selection based on a
+	 * newly created image in the devices camera application
+	 */
+	private void cameraIntent() {
+		String fileName = "ias-" + System.currentTimeMillis() + ".jpg";
+		ContentValues values = new ContentValues();
+		values.put(MediaStore.Images.Media.TITLE, fileName);
+		values.put(MediaStore.Images.Media.DESCRIPTION, "Taken for invadr");
+		lastCreatedImageUri = _activity.getContentResolver().insert(
+				MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+		// create intent with extra output to grab uri later
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, lastCreatedImageUri);
+
+		_activity.startActivityForResult(
+				Intent.createChooser(intent, "Select Picture"), CAMERA_OPTION);
+	}
+
+	/**
+	 * Creates and executes and Intent to capture and process an image selection
+	 * from the users gallery/device
+	 */
+	private void galleryIntent() {
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+
+		_activity.startActivityForResult(intent, GALLERY_OPTION);
+	}
+
 }
--- Ias-AshTag/src/org/iasess/ashtag/handlers/ClickHandler.java
+++ Ias-AshTag/src/org/iasess/ashtag/handlers/ClickHandler.java
@@ -1,23 +1,14 @@
 package org.iasess.ashtag.handlers;
 
 import org.iasess.ashtag.AshTagApp;
-import org.iasess.ashtag.R;
+import org.iasess.ashtag.ImageHandler;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.MediaStore;
 
 import com.actionbarsherlock.view.MenuItem;
 
 public class ClickHandler {
 
-	public static final int CAMERA_OPTION = 1000;
-	public static final int GALLERY_OPTION = 1001;
-	
 	private Activity _activity;
 	
 	public ClickHandler(Activity activity){
@@ -25,43 +16,9 @@ public class ClickHandler {
 	}
 	public void onAddSightingClick(MenuItem mi) {
     	if(!(android.util.Patterns.EMAIL_ADDRESS.matcher(AshTagApp.getUsernamePreferenceString()).matches())){
-    		AlertDialog.Builder builder = new AlertDialog.Builder(_activity);
-    		// set items from resource list
-    		String[] options = _activity.getResources().getStringArray(R.array.camera_options);
-    		builder.setItems(options, new DialogInterface.OnClickListener() {
-    			public void onClick(DialogInterface dialog, int item) {
-    				if (item == 0) cameraIntent();
-    				else galleryIntent();
-    			}
-    		});
-    		builder.create().show();
+        	new ImageHandler(_activity).showChooser();
     	} else {
     		//TODO: ask for email address
     	}
     } 
-	
-	private void cameraIntent() {
-		Uri lastCreatedImageUri;
-		String fileName = "ias-" + System.currentTimeMillis() + ".jpg";
-		ContentValues values = new ContentValues();
-		values.put(MediaStore.Images.Media.TITLE, fileName);
-		values.put(MediaStore.Images.Media.DESCRIPTION, "Taken for invadr");
-		lastCreatedImageUri = _activity.getContentResolver().insert(
-				MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-		// create intent with extra output to grab uri later
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, lastCreatedImageUri);
-
-		_activity.startActivityForResult(
-				Intent.createChooser(intent, "Select Picture"), CAMERA_OPTION);
-	}
-
-	private void galleryIntent() {
-		Intent intent = new Intent();
-		intent.setType("image/*");
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-
-		_activity.startActivityForResult(intent, GALLERY_OPTION);
-	}
 }
